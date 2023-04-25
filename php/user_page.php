89511061da09f60a59e1696a5beb56ec47556854
PaoloBud <budurca.1934644@studenti.uniroma1.it>
2023-04-26 00:04:47 +0200

    dd

--- php/user_page.php
+++ php/user_page.php
@@ -25,7 +25,7 @@
       <h3>hi, <span> buddy</span></h3>
      
       <p>this is an user page</p>
-      <a href="login_form.php" class="btn">login</a>
+      <a href="../html/shop.html" class="btn">shop</a>
       <a href="register_form.php" class="btn">register</a>
       <a href="logout.php" class="btn">logout</a>
    </div>
