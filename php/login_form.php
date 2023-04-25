f995ebcdc5eed3fa4c4501c753fd7055c96262f3
PaoloBud <budurca.1934644@studenti.uniroma1.it>
2023-04-25 21:42:28 +0200

    css

--- php/login_form.php
+++ php/login_form.php
@@ -48,7 +48,7 @@ if(isset($_POST['submit'])){
    <title>login form</title>
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="GymBros\.utils\css\login.css">
+   <link rel="stylesheet" href="css/login.css">
 
 </head>
 <body>
