dd03fd27d72ca43fbb1e8fb0e0b22a5feb965473
PaoloBud <budurca.1934644@studenti.uniroma1.it>
2023-04-27 22:45:13 +0200

    df

--- php/register_form.php
+++ php/register_form.php
@@ -1,13 +1,10 @@
 <script>
 function myFunction() {
   var x = document.getElementById("myInput");
-  var y= document.getElementById("myInput");
-  if (x.type === "password" && y.type ==="password") {
+  if (x.type === "password") {
     x.type = "text";
-    y.type="text"
   } else {
     x.type = "password";
-    y.type="password"
   }
 }
 </script>
