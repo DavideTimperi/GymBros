9a04133e0477ff59385c88416c1be8cddd41ec0c
TimperiDavide <timperi.1950722@studenti.uniroma1.it>
2023-04-27 11:19:54 +0200

    checkout

--- js/checkout.js
+++ js/checkout.js
@@ -1,88 +1,56 @@
-'use strict';
-
-// all initial elements
-const payAmountBtn = document.querySelector('#payAmount');
-const decrementBtn = document.querySelectorAll('#decrement');
-const quantityElem = document.querySelectorAll('#quantity');
-const incrementBtn = document.querySelectorAll('#increment');
-const priceElem = document.querySelectorAll('#price');
-const subtotalElem = document.querySelector('#subtotal');
-const taxElem = document.querySelector('#tax');
-const totalElem = document.querySelector('#total');
-
-
-// loop: for add event on multiple `increment` & `decrement` button
-for (let i = 0; i < incrementBtn.length; i++) {
-
-  incrementBtn[i].addEventListener('click', function () {
-
-    // collect the value of `quantity` textContent,
-    // based on clicked `increment` button sibling.
-    let increment = Number(this.previousElementSibling.textContent);
-
-    // plus `increment` variable value by 1
-    increment++;
-
-    // show the `increment` variable value on `quantity` element
-    // based on clicked `increment` button sibling.
-    this.previousElementSibling.textContent = increment;
-
-    totalCalc();
-
-  });
-
-
-  decrementBtn[i].addEventListener('click', function () {
-
-    // collect the value of `quantity` textContent,
-    // based on clicked `decrement` button sibling.
-    let decrement = Number(this.nextElementSibling.textContent);
-
-    // minus `decrement` variable value by 1 based on condition
-    decrement <= 1 ? 1 : decrement--;
-
-    // show the `decrement` variable value on `quantity` element
-    // based on clicked `decrement` button sibling.
-    this.nextElementSibling.textContent = decrement;
-
-    totalCalc();
-
-  });
-
-}
-
-
-
-// function: for calculating total amount of product price
-const totalCalc = function () {
-
-  // declare all initial variable
-  const tax = 0.05;
-  let subtotal = 0;
-  let totalTax = 0;
+/*
+Generate html from php database
+*/
+window.onload = function() {
+  const rows = 0;
+  const title = $("<h3/>", {
+    text: "Cart"
+  })
+  const manipulate = $(".product-summary").append([title])
   let total = 0;
+  let linked
+  let div
+  let txt
+  let go_to_shop
+  if (rows > 0) {
+      for (let i=0; i<rows; i++){
+        
+        txt = "ITEM IMAGE ------ ITEM NAME ------- QNTITY ------ PRICE TOTAL "
+        div = $("<div/>", {
+          text: txt,
+          class: "summary",     
+        }); 
+        
+        manipulate.append(div)
 
-  // loop: for calculating `subtotal` value from every single product
-  for (let i = 0; i < quantityElem.length; i++) {
-
-    subtotal += Number(quantityElem[i].textContent) * Number(priceElem[i].textContent);
-
+      } 
+      manipulate.append($("<hr/>").attr('style', 'background-color:rgb(28, 28, 28, 0.8) !important;'))       
+      manipulate.append(total)
+  }
+  else {
+    linked = "shop.html";
+    let empty_cart = $("<div/>", {
+      text: "Your cart is empty",
+      align: "center",
+    }); 
+    go_to_shop = $("<button/>", {
+      text: "go to shop", 
+      class: "redirect-button",
+    }); 
+    let link_anchor = $('<a>',{
+      href: linked,
+      append: go_to_shop
+   });
+    manipulate.append([empty_cart, link_anchor])
   }
 
-  // show the `subtotal` variable value on `subtotalElem` element
-  subtotalElem.textContent = subtotal.toFixed(2);
-
-  // calculating the `totalTax`
-  totalTax = subtotal * tax;
-
-  // show the `totalTax` on `taxElem` element
-  taxElem.textContent = totalTax.toFixed(2);
-
-  // calcualting the `total`
-  total = subtotal + totalTax;
-
-  // show the `total` variable value on `totalElem` & `payAmountBtn` element
-  totalElem.textContent = total.toFixed(2);
-  payAmountBtn.textContent = total.toFixed(2);
-
 }
+
+$(document).ready(function() {
+  $('#hide-check').change(function(){
+    if(this.checked)
+        $('#shipping').fadeOut('fast');
+    else
+        $('#shipping').fadeIn('fast');
+  });
+});
