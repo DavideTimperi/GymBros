02db270862115e305a0f950dd40acb706368de0a
PaoloBud <budurca.1934644@studenti.uniroma1.it>
2023-05-08 19:11:10 +0200

    df

--- php/shop_index.php
+++ php/shop_index.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+
 ?>
 
 <!DOCTYPE html>
@@ -9,6 +10,7 @@ session_start();
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name = "viewport" content = "width=device-width, minimum-scale=1.0, maximum-scale = 1.0, user-scalable = no">
   
   <title>Shop</title>
 
@@ -23,10 +25,12 @@ session_start();
 </head>
 
 <body>
+
+<!-- first header
   <header>
     <div class="navbar">
-      <a href="../html/cart.html">
-        <h2>Clothing Store</h2>
+      <a href="home.html">
+        <h2>Gymbros</h2>
       </a>
   
       <a href="../html/cart.html">
@@ -38,6 +42,34 @@ session_start();
   
     </div>
   </header>
+-->
+
+<header class="scroll">
+  <nav class="headerWraper layout">
+      <div class="logo">
+          <a href="home.html"> <b>Gymbros</b> </a>
+      </div>
+      <div>
+          <ul class="nav">
+              <li><a href="home.html" class="nav-link">Home</a></li>
+              <li><a href="shop.html" class="nav-link link-active">Shop</a></li>
+              <li><a href="staff.html" class="nav-link">Staff</a></li>
+              <li><a href="../php/login_form.php"><i class="fa-solid fa-user nav-link"></i></a></li>
+              <a href="../html/cart.html">
+                <div class="cart">
+                  <i class="fa-solid fa-shopping-cart"></i>
+                  <div id="cartAmount" class="cartAmount">0</div>
+                </div>
+              </a>
+          </ul>
+      </div>
+
+      <div class="menu">
+          <i class="bx bx-menu bx-md"></i>
+      </div>
+  </nav>
+</header>
+
 
   <div class="title-shop">
     <h2>SHOP</h2>
@@ -109,5 +141,6 @@ session_start();
 </body>
 <script src="../js/Data.js"></script>
 <script src="../js/shop.js"></script>
+<script src="../js/main.js"></script>
 
 </html>
