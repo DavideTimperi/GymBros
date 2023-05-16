042c8c7c6cd5f97ba8d38147edf9d4b82e4f33f4
PaoloBud <budurca.1934644@studenti.uniroma1.it>
2023-05-17 00:32:54 +0200

    fdsf

--- php/user_page.php
+++ php/user_page.php
@@ -79,7 +79,7 @@ if(!isset($_SESSION['email'])){
       <!--seconda card-->
 
       <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="../.utils/images/payment.png" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">CHECKOUT</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -98,7 +98,7 @@ if(!isset($_SESSION['email'])){
     <div class="col">
       <!--terza card-->
       <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="../.utils/images/logout.png" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">LOGOUT</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
