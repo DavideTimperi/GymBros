715eee96eb31755d558d5866460212256a3a919f
TimperiDavide <timperi.1950722@studenti.uniroma1.it>
2023-05-17 10:54:19 +0200

    fix

--- php/user_page.php
+++ php/user_page.php
@@ -86,15 +86,15 @@ if(!isset($_SESSION['email'])){
 
     <div class="col">
       <!--terza card-->
-      <a href="logout.php" class="btn btn-light" ><div class="card pippo" style="width: 18rem;">
-  <img src="../.utils/images/logout.png" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">LOGOUT</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  
+      <a href="logout.php" class="btn btn-light" >
+         <div class="card pippo" style="width: 18rem;">
+            <img src="../.utils/images/logout.png" class="card-img-top" alt="...">
+            <div class="card-body">
+               <h5 class="card-title">LOGOUT</h5>
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+         </div></a>
    </div>
-  </div></a>
-</div>
 
 
 
